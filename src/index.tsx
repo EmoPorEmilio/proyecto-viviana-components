d49259dd168b14d381b068cd6a92fddaa41f126d
EmoPorEmilio <franceschini.emilio@gmail.com>
2024-07-19 20:38:41 -0300

    add tailwind export

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,5 @@ import Header from './Header'
 import {Alert, AlertType} from './Alert'
 import {Button, ButtonType} from './Button';
 import Chip from './Chip'
-
-export { LateralNav, Header, Alert, AlertType, Button, ButtonType, Chip }
+import TailwindStyles from '../tailwindcss.config'
+export { LateralNav, Header, Alert, AlertType, Button, ButtonType, Chip, TailwindStyles }
