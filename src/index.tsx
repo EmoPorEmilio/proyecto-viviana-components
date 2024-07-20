63bf201b3a60fcc6cbdf960ec5fdd775a77051bd
EmoPorEmilio <franceschini.emilio@gmail.com>
2024-07-19 21:40:13 -0300

    .

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,5 @@ import Header from './Header'
 import {Alert, AlertType} from './Alert'
 import {Button, ButtonType} from './Button';
 import Chip from './Chip'
-import {mainTheme} from '../tailwindcss.config'
+import {mainTheme} from './tailwindcss.config'
 export { LateralNav, Header, Alert, AlertType, Button, ButtonType, Chip, mainTheme }
