c5ff6d75914ce5f8b867f8d17a5e6ff62d0d9839
EmoPorEmilio <franceschini.emilio@gmail.com>
2024-07-31 20:38:19 -0300

    change animation

--- src/tailwindcss/index.ts
+++ src/tailwindcss/index.ts
@@ -2,8 +2,10 @@ const mainTheme = {
   extend: {
     keyframes: {
       wiggle: {
-        '0%, 100%': { transform: 'rotate(-3deg)' },
-        '50%': { transform: 'rotate(3deg)' },
+        '0%, 100%': { transform: 'rotate(0deg)' },
+        '25': { transform: 'rotate(3deg)' },
+        '50%': { transform: 'rotate(0deg)' },
+        '75': { transform: 'rotate(-3deg)' },
       },
     },
     animation: {
